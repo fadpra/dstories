9c3c1cbd1985c93bb52a613f0b5c229972aa1544
fadpra <74486488+fadpra@users.noreply.github.com>
2023-08-16 19:24:53 +0700

    Add files via upload

---
+++ homepage/admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>Welcome to D'Stories Admin Page!</h1>
+        <h2>Select what you want to do:</h2>
+        <a href="">Add Product</a><br>
+        <a href="">Delete Product</a><br>
+        <a href="">Account List</a><br>
+        <a href="homepage/index.php">D'Stories Homepage</a>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
---
+++ homepage/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <nav>
+            <div class="dstories"><a href="#">D'Stories</a></div>
+            <div class="hid1"></div>
+            <div class="hid2"></div>
+            <div class="links">
+                <a href="../shop/shop.php">Shop</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact Us</a>
+                <!--<a class="fa-brands fa-whatsapp" href="https://wa.me/628111027849" target="_blank"></a>-->
+            </div>
+        </nav><br>
+
+        <div class="hero">
+            <div class="hero-content">
+                <h1 class="hero-text">We are D'stories Indonesia</h1>
+                <h2 class="hero-text">Start Your Journey With Us Today!</h2><br>
+                <a href="../shop/shop.php">Shop Here</a>
+            </div>
+        </div>
+
+        <div id="gra1"></div>
+
+        <div class="catus">
+            <div class="container">
+                <h1>Shop By Category</h1>
+                <div class="category">
+                    <a href="shop.php" class="product" id="bagcat">
+                        <div>
+                            <img src="dstories category image.png" alt="">
+                            <h2>Bags & Pouches</h2>
+                            <h3>Shop Here</h3>
+                        </div>
+                    </a>
+                    <a href="shop.php" class="product" id="homelinens">
+                        <div>
+                            <img src="homelinens.png" alt="">
+                            <h2>Home Linens</h2>
+                            <h3>Shop Here</h3>
+                        </div>
+                    </a>
+                    <a href="shop.php" class="product" id="stationary">
+                        <div>
+                            <img src="2.png" alt="">
+                            <h2>Stationary</h2>
+                            <h3>Shop Here</h3>
+                        </div>
+                    </a>
+                    <a href="shop.php" class="product" id="personal">
+                        <div>
+                            <img src="3.png" alt="">
+                            <h2>Personal Products</h2>
+                            <h3>Shop Here</h3>
+                        </div>
+                    </a>
+                    <a href="shop.php" class="product" id="beyond">
+                        <div>
+                            <img src="4.png" alt="">
+                            <h2>And Beyond!</h2>
+                            <h3>Shop Here</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div id="about"></div>
+
+            <div class="about">
+                <h1>About D'Stories</h1>
+                <div class="about-con">
+                    <div class="about-text">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Vivamus tempor dictum tortor, id tincidunt mi egestas malesuada. 
+                            Curabitur lobortis condimentum quam, ut tempus sem finibus ut. 
+                            Mauris id nunc tincidunt, feugiat lorem eu, ornare quam. Suspendisse 
+                            tincidunt nulla ut dolor tempus, eu varius arcu pretium. Maecenas 
+                            congue dolor sed semper tempus. Mauris porta feugiat ipsum ut malesuada. 
+                            Donec euismod semper purus, pretium interdum nibh aliquam id. Ut semper 
+                            velit vitae est venenatis lacinia. Mauris rhoncus diam vel suscipit 
+                            consectetur. Pellentesque at erat metus. Aenean tincidunt congue convallis.
+                            Donec euismod semper purus, pretium interdum nibh aliquam id. Ut semper 
+                            velit vitae est venenatis lacinia. Mauris rhoncus diam vel suscipit 
+                            consectetur. Pellentesque at erat metus. Aenean tincidunt congue convallis.
+                            Donec euismod semper purus
+                        </h2>
+                    </div>
+                    <div class="about-img">
+                        <img src="aboutusimg.png" alt="">
+                    </div>
+                    <!-- abt content div closer -->
+                </div>
+                <!-- abt div closer closer -->
+            </div>
+        <!-- content div closer -->
+        </div>
+
+        
+        <div id="contact"></div>
+        <footer>
+            <h2>Our Contacts</h2>
+            <div class="contacts">
+                <a class="instalink" href="https://www.instagram.com/dstories.id/" target="_blank"><h3>Instagram - dstories.id</h3></a>
+                <a href="https://www.tokopedia.com/decorstories" target="_blank"><h3>Tokopedia - DStories</h3></a>
+                <a href="https://wa.me/628192230315" target="_blank"><h3>Whatsapp - +62 819 2230 315</h3></a>
+                <h3>Email - dstories.id@gmail.com</h3>
+            </div>
+            <h5>Website made by <a href="https://www.youtube.com/channel/UCwOr3EV5QHTYQOH0r7TCljA" target="_blank">Fadil Surya Pramana</a></h5>
+        </footer>
+        
+        <script src="" async defer></script>
+        <script src="https://kit.fontawesome.com/9fc4b69795.js" crossorigin="anonymous"></script>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/649bf100cc26a871b0251d19/1h40iarcr';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+            </script>
+        <!--End of Tawk.to Script-->
+        <script src="" async defer></script>
+    </body>
+</html>
